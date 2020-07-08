93cf31adae77b5a668c958515d89cad744d83b7c
DanielDavidyan <Daniel3153>
2020-07-08 19:15:06 +0300

    fomantic 1.1

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,21 @@
-$(document).ready(function(){
-    //in index - for the rating stars
+$(document).ready(function () {
     $('.ui.rating')
         .rating({
             icon: 'star',
             initialRating: 3,
             maxRating: 5
-        })
-    ;
-    //in cart - for the amount of product (dropdown)
+        });
     $('.ui.dropdown.selection')
         .dropdown({
             ignoreDiacritics: true,
             sortSelect: true,
-            fullTextSearch:'exact'
-        })
-    ;
-    //in index - for the popup menu (account&lists)
+            fullTextSearch: 'exact'
+        });
     $('.browse.item')
         .popup({
             delay: {
                 show: 300,
                 hide: 1500
             }
-        })
-    ;
+        });
 });
